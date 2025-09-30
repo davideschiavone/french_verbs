ee0c1d0a1f8819e0dcc86d096b18b589147a9421
davide schiavone <davide@openhwgroup.org>
2025-09-30 09:43:16 +0200

    add imparfait

--- verbs.py
+++ verbs.py
@@ -4,82 +4,97 @@ verbs = {
     "etre": {
         "presente": ["je suis", "tu es", "il est", "nous sommes", "vous etes", "ils sont"],
         "futuro": ["je serai", "tu seras", "il sera", "nous serons", "vous serez", "ils seront"],
-        "passe": ["j'ai ete", "tu as ete", "il a ete", "nous avons ete", "vous avez ete", "ils ont ete"]
+        "passe": ["j'ai ete", "tu as ete", "il a ete", "nous avons ete", "vous avez ete", "ils ont ete"],
+        "imparfait": ["j'etais", "tu etais", "il etait", "nous etions", "vous etiez", "ils etaient"]
     },
     "avoir": {
         "presente": ["j'ai", "tu as", "il a", "nous avons", "vous avez", "ils ont"],
         "futuro": ["j'aurai", "tu auras", "il aura", "nous aurons", "vous aurez", "ils auront"],
-        "passe": ["j'ai eu", "tu as eu", "il a eu", "nous avons eu", "vous avez eu", "ils ont eu"]
+        "passe": ["j'ai eu", "tu as eu", "il a eu", "nous avons eu", "vous avez eu", "ils ont eu"],
+        "imparfait": ["j'avais", "tu avais", "il avait", "nous avions", "vous aviez", "ils avaient"]
     },
     "aller": {
         "presente": ["je vais", "tu vas", "il va", "nous allons", "vous allez", "ils vont"],
         "futuro": ["j'irai", "tu iras", "il ira", "nous irons", "vous irez", "ils iront"],
-        "passe": ["je suis alle", "tu es alle", "il est alle", "nous sommes alles", "vous etes alles", "ils sont alles"]
+        "passe": ["je suis alle", "tu es alle", "il est alle", "nous sommes alles", "vous etes alles", "ils sont alles"],
+        "imparfait": ["j'allais", "tu allais", "il allait", "nous allions", "vous alliez", "ils allaient"]
     },
     "venir": {
         "presente": ["je viens", "tu viens", "il vient", "nous venons", "vous venez", "ils viennent"],
         "futuro": ["je viendrai", "tu viendras", "il viendra", "nous viendrons", "vous viendrez", "ils viendront"],
-        "passe": ["je suis venu", "tu es venu", "il est venu", "nous sommes venus", "vous etes venus", "ils sont venus"]
+        "passe": ["je suis venu", "tu es venu", "il est venu", "nous sommes venus", "vous etes venus", "ils sont venus"],
+        "imparfait": ["je venais", "tu venais", "il venait", "nous venions", "vous veniez", "ils venaient"]
     },
     "prendre": {
         "presente": ["je prends", "tu prends", "il prend", "nous prenons", "vous prenez", "ils prennent"],
         "futuro": ["je prendrai", "tu prendras", "il prendra", "nous prendrons", "vous prendrez", "ils prendront"],
-        "passe": ["j'ai pris", "tu as pris", "il a pris", "nous avons pris", "vous avez pris", "ils ont pris"]
+        "passe": ["j'ai pris", "tu as pris", "il a pris", "nous avons pris", "vous avez pris", "ils ont pris"],
+        "imparfait": ["je prenais", "tu prenais", "il prenait", "nous prenions", "vous preniez", "ils prenaient"]
     },
     "partir": {
         "presente": ["je pars", "tu pars", "il part", "nous partons", "vous partez", "ils partent"],
         "futuro": ["je partirai", "tu partiras", "il partira", "nous partirons", "vous partirez", "ils partiront"],
-        "passe": ["je suis parti", "tu es parti", "il est parti", "nous sommes partis", "vous etes partis", "ils sont partis"]
+        "passe": ["je suis parti", "tu es parti", "il est parti", "nous sommes partis", "vous etes partis", "ils sont partis"],
+        "imparfait": ["je partais", "tu partais", "il partait", "nous partions", "vous partiez", "ils partaient"]
     },
     "voir": {
         "presente": ["je vois", "tu vois", "il voit", "nous voyons", "vous voyez", "ils voient"],
         "futuro": ["je verrai", "tu verras", "il verra", "nous verrons", "vous verrez", "ils verront"],
-        "passe": ["j'ai vu", "tu as vu", "il a vu", "nous avons vu", "vous avez vu", "ils ont vu"]
+        "passe": ["j'ai vu", "tu as vu", "il a vu", "nous avons vu", "vous avez vu", "ils ont vu"],
+        "imparfait": ["je voyais", "tu voyais", "il voyait", "nous voyions", "vous voyiez", "ils voyaient"]
     },
     "faire": {
         "presente": ["je fais", "tu fais", "il fait", "nous faisons", "vous faites", "ils font"],
         "futuro": ["je ferai", "tu feras", "il fera", "nous ferons", "vous ferez", "ils feront"],
-        "passe": ["j'ai fait", "tu as fait", "il a fait", "nous avons fait", "vous avez fait", "ils ont fait"]
+        "passe": ["j'ai fait", "tu as fait", "il a fait", "nous avons fait", "vous avez fait", "ils ont fait"],
+        "imparfait": ["je faisais", "tu faisais", "il faisait", "nous faisions", "vous faisiez", "ils faisaient"]
     },
     "dire": {
         "presente": ["je dis", "tu dis", "il dit", "nous disons", "vous dites", "ils disent"],
         "futuro": ["je dirai", "tu diras", "il dira", "nous dirons", "vous direz", "ils diront"],
-        "passe": ["j'ai dit", "tu as dit", "il a dit", "nous avons dit", "vous avez dit", "ils ont dit"]
+        "passe": ["j'ai dit", "tu as dit", "il a dit", "nous avons dit", "vous avez dit", "ils ont dit"],
+        "imparfait": ["je disais", "tu disais", "il disait", "nous disions", "vous disiez", "ils disaient"]
     },
     "pouvoir": {
         "presente": ["je peux", "tu peux", "il peut", "nous pouvons", "vous pouvez", "ils peuvent"],
         "futuro": ["je pourrai", "tu pourras", "il pourra", "nous pourrons", "vous pourrez", "ils pourront"],
-        "passe": ["j'ai pu", "tu as pu", "il a pu", "nous avons pu", "vous avez pu", "ils ont pu"]
+        "passe": ["j'ai pu", "tu as pu", "il a pu", "nous avons pu", "vous avez pu", "ils ont pu"],
+        "imparfait": ["je pouvais", "tu pouvais", "il pouvait", "nous pouvions", "vous pouviez", "ils pouvaient"]
     },
     "vouloir": {
         "presente": ["je veux", "tu veux", "il veut", "nous voulons", "vous voulez", "ils veulent"],
         "futuro": ["je voudrai", "tu voudras", "il voudra", "nous voudrons", "vous voudrez", "ils voudront"],
-        "passe": ["j'ai voulu", "tu as voulu", "il a voulu", "nous avons voulu", "vous avez voulu", "ils ont voulu"]
+        "passe": ["j'ai voulu", "tu as voulu", "il a voulu", "nous avons voulu", "vous avez voulu", "ils ont voulu"],
+        "imparfait": ["je voulais", "tu voulais", "il voulait", "nous voulions", "vous vouliez", "ils voulaient"]
     },
     "savoir": {
         "presente": ["je sais", "tu sais", "il sait", "nous savons", "vous savez", "ils savent"],
         "futuro": ["je saurai", "tu sauras", "il saura", "nous saurons", "vous saurez", "ils sauront"],
-        "passe": ["j'ai su", "tu as su", "il a su", "nous avons su", "vous avez su", "ils ont su"]
+        "passe": ["j'ai su", "tu as su", "il a su", "nous avons su", "vous avez su", "ils ont su"],
+        "imparfait": ["je savais", "tu savais", "il savait", "nous savions", "vous saviez", "ils savaient"]
     },
     "rentrer": {
         "presente": ["je rentre", "tu rentres", "il rentre", "nous rentrons", "vous rentrez", "ils rentrent"],
         "futuro": ["je rentrerai", "tu rentreras", "il rentrera", "nous rentrerons", "vous rentrerez", "ils rentreront"],
-        "passe": ["je suis rentre", "tu es rentre", "il est rentre", "nous sommes rentres", "vous etes rentres", "ils sont rentres"]
+        "passe": ["je suis rentre", "tu es rentre", "il est rentre", "nous sommes rentres", "vous etes rentres", "ils sont rentres"],
+        "imparfait": ["je rentrais", "tu rentrais", "il rentrait", "nous rentrions", "vous rentriez", "ils rentraient"]
     },
     "manger": {
         "presente": ["je mange", "tu manges", "il mange", "nous mangeons", "vous mangez", "ils mangent"],
         "futuro": ["je mangerai", "tu mangeras", "il mangera", "nous mangerons", "vous mangerez", "ils mangeront"],
-        "passe": ["j'ai mange", "tu as mange", "il a mange", "nous avons mange", "vous avez mange", "ils ont mange"]
+        "passe": ["j'ai mange", "tu as mange", "il a mange", "nous avons mange", "vous avez mange", "ils ont mange"],
+        "imparfait": ["je mangeais", "tu mangeais", "il mangeait", "nous mangions", "vous mangiez", "ils mangeaient"]
     },
     "aimer": {
         "presente": ["j'aime", "tu aimes", "il aime", "nous aimons", "vous aimez", "ils aiment"],
         "futuro": ["j'aimerai", "tu aimeras", "il aimera", "nous aimerons", "vous aimerez", "ils aimeront"],
-        "passe": ["j'ai aime", "tu as aime", "il a aime", "nous avons aime", "vous avez aime", "ils ont aime"]
+        "passe": ["j'ai aime", "tu as aime", "il a aime", "nous avons aime", "vous avez aime", "ils ont aime"],
+        "imparfait": ["j'aimais", "tu aimais", "il aimait", "nous aimions", "vous aimiez", "ils aimaient"]
     }
 }
 
 persons = ['je', 'tu', 'il', 'nous', 'vous', 'ils']
-tenses = ['presente', 'futuro', 'passe']
+tenses = ['presente', 'futuro', 'passe', 'imparfait']
 
 def generate_question_deck():
     deck = []
